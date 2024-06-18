ecf9b70ff3bfa9b6bfa2792ec34573f52d0972ad
PopovNikita <popovn@reshenie-soft.ru>
2024-06-19 01:11:26 +0300

    [+] Добавил описание моделей User и Film [+] Добавил зависимость spring-boot-starter-validation

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,28 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.*;
+import lombok.*;
 
-/**
- * Film.
- */
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 public class Film {
+    private int id;
+
+    @NotBlank(message = "Название не может быть пустым")
+    private String name;
+
+    @Size(max = 200, message = "Максимальная длина описания — 200 символов")
+    private String description;
+
+    @NotNull(message = "Дата релиза не может быть пустой")
+    @PastOrPresent(message = "Дата релиза не может быть в будущем")
+    private LocalDate releaseDate;
+
+    @Positive(message = "Продолжительность фильма должна быть положительным числом")
+    private int duration;
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,29 @@
+package ru.yandex.practicum.filmorate.model;
+
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+    private int id;
+
+    @NotBlank(message = "Электронная почта не может быть пустой")
+    @Email(message = "Электронная почта должна содержать символ @")
+    private String email;
+
+    @NotBlank(message = "Логин не может быть пустым")
+    @Pattern(regexp = "\\S+", message = "Логин не может содержать пробелы")
+    private String login;
+
+    private String name;
+
+    @NotNull(message = "Дата рождения не может быть пустой")
+    @Past(message = "Дата рождения не может быть в будущем")
+    private LocalDate birthday;
+}
